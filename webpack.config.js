c9c351b6aa4a2244d576e0512ac029b1fc885424
Christian Niedermayer <sdchrisn@gmail.com>
2019-03-10 23:58:27 -0700

    Cleaned up unnecessary webpack sass loader

--- webpack.config.js
+++ webpack.config.js
@@ -30,7 +30,7 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
+        use: ['style-loader', 'css-loader'],
       }
     ]
   }
