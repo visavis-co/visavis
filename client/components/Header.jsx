704992360463d0ecf8ca62c721c0052d459c8393
Nisha <nishakchatterjee@gmail.com>
2019-03-11 17:07:30 -0700

    Nav bar semi functional

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -1,10 +1,36 @@
 import React from 'react';
+import { Image, Navbar, Form, Dropdown, Nav, Button } from 'react-bootstrap';
 
 const Header = props => {
+    const assets = '/client/assets/';
+    const myPic = assets + props.userInfo.pictureurl;
+    const visavis = assets + 'visavis.jpg';
     return (
-        <div className='nav'>
-        
-        </div>
+        <Navbar className="nav" expand="lg">
+            <Navbar.Brand ><Image src={visavis} width='40%' /></Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav
+                    activeKey="/home"
+                    onSelect={selectedKey => alert(`selected ${selectedKey}`)}
+                >
+                    <Form inline className="justify-content-end">
+                        <Dropdown>
+                            <Dropdown.Toggle className="m-3" variant="outline-dark" id="dropdown-basic">
+                                <Image src={myPic} width="10%" height='auto' className="roundedCircle" />
+                            </Dropdown.Toggle>
+                            <Dropdown.Menu>
+                                <Dropdown.Item href="#/action-1">Profile</Dropdown.Item>
+                                <Dropdown.Item href="#/action-2">Settings</Dropdown.Item>
+                                <Dropdown.Item href="#/action-3">Past Matches</Dropdown.Item>
+                            </Dropdown.Menu>
+                        </Dropdown>
+                        <Button className="m-3" variant="outline-dark">Current Match</Button>
+                        <Button className="m-3" variant="outline-dark" onClick={props.userLogout}>Logout</Button>
+                    </Form>
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
     )
 };
 
--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -1,25 +1,38 @@
 import React from 'react';
+import { Button, Image, Container, Row, Col, Jumbotron } from 'react-bootstrap';
 
 // private route
 // what a user sees when they go to "/" and are logged in
 // this month's match is...
 
 const Home = props => {
-    console.log(props.currentMatch)
-    // const current = props.currentMatch;
-    // const past = props.pastMatches;
-    // const history = [];
-    // for(let i = 0; i < data.length; i++){
-    //     history.push(<Match user={data[i]} />);
-    // }
+    const assets = '/client/assets/';
+
+    const current = props.currentMatch;
+    const matchPic = assets + current.pictureurl;
+
+    const history = [];
+    for (let i = 0; i < props.pastMatches.length; i++) {
+        history.push(props.pastMatches[i].fullname);
+    }
 
     return (
         <div>
-            <h1>Welcome, {props.userInfo.fullname}</h1>
-            {/* {current} */}
-            <h3>Past Matches</h3>
-            {/* {past} */}
-            <button onClick={props.userLogout}>Log Out</button>
+            <Jumbotron>
+                <Container>
+                    <h1>Welcome, {props.userInfo.fullname}</h1>
+                    <Row>Your match for this week is...</Row>
+                    <Image src={matchPic} className="img-fluid rounded img-thumbnail" width="40%" height="auto" alt="Match's profile pic" />
+                    <Row>{current.fullname}</Row>
+                </Container>
+            </Jumbotron>
+
+            <Jumbotron>
+                <Container>
+                    <h3>Past Matches</h3>
+                    {history}
+                </Container>
+            </Jumbotron>
         </div>
     );
 }
