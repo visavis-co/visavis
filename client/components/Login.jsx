c6102e5ec0ee928619c9f2baae38e860e7c9a735
Chris Air <sdchrisn@gmail.com>
2019-03-11 18:44:40 -0700

    corrected the create account button

--- client/components/Login.jsx
+++ client/components/Login.jsx
@@ -6,26 +6,6 @@ import { Link } from 'react-router-dom';
 const Login = props => {
   return (
     <div className='formLogin'>
-<<<<<<< HEAD
-      {/* <div className='loader'></div> */}
-    <Form>
-      <Form.Group controlId='formEmail'>
-        <Form.Control type='email' placeholder='Enter email' onChange={props.enterEmail} />
-      </Form.Group>
-      <Form.Group controlId='formPassword'>
-        <Form.Control type='password' placeholder='Password' onChange={props.enterPassword} />
-      </Form.Group>
-      <div>
-      <Button  variant='primary' type='submit' className='btnLogin'
-        onClick={(e) => {e.preventDefault(); props.userLogin(props.email, props.password)}}>Log in</Button> 
-      </div>
-      <a href="/auth/github" className="btn btn-block btn-social btn-github">
-        <FontAwesomeIcon className='picGithub' size="2x" icon={['fab', 'github']} />
-          Sign in with Github
-      </a>
-      <Button variant='primary'><Link to="/signup" className="linkButton" >Create an account</Link></Button> 
-    </Form>
-=======
       <img src='./../client/assets/visavis.jpg' width='250'></img>
       <Form id='login-form'>
         <Form.Group controlId='formEmail'>
@@ -37,10 +17,9 @@ const Login = props => {
         <div id='login-buttons'>
           <Button id='login-button' variant='primary' type='submit' className='btnLogin' onClick={(e) => {e.preventDefault(); props.userLogin(props.email, props.password)}}>Log in</Button>
           <a id='login-github' href="/auth/github" className="btn btn-block btn-social btn-github"><FontAwesomeIcon className='picGithub' size="2x" icon={['fab', 'github']} /> Sign in with Github </a>
-          <Button id='create-account' variant='primary' href="/signup">Create an account</Button>
+          <Button id='create-account' variant='primary'><Link to="/signup" className="linkButton">Create an account</Link></Button>
         </div>
       </Form>
->>>>>>> 6063ae493dcd19572797fa5f2723e3266d3780b9
     </div>
   );
 };
